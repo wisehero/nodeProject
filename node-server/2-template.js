de36600aa9f90caea733bddbe659c20d461c7201
김지웅 <godjw96@gmail.com>
2021-06-17 19:26:10 +0900

    Create 2-template.js

---
+++ node-server/2-template.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+const ejs = require("ejs");
+
+const name = "JiWoong";
+const courses = [
+  { name: "HTML" },
+  { name: "CSS" },
+  { name: "JS" },
+  { name: "Node" },
+  { name: "Frontend" },
+];
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  res.setHeader("Content-Type", "text/html");
+  if (url === "/") {
+    ejs
+      .renderFile("./template/index.ejs", { name })
+      .then((data) => res.end(data));
+  } else if (url === "/courses") {
+    ejs
+      .renderFile("./template/courses.ejs", { courses })
+      .then((data) => res.end(data));
+  } else {
+    ejs
+      .renderFile("./template/not-found.ejs", { name })
+      .then((data) => res.end(data));
+  }
+});
+
+server.listen(8000);

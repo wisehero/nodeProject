b96f37f8b61836f6ca706dcf6739fed3c342863a
김지웅 <godjw96@gmail.com>
2021-07-05 02:50:18 +0900

    multer

--- nodeWorkbook/learn-express/app.js
+++ nodeWorkbook/learn-express/app.js
@@ -9,11 +9,17 @@ dotenv.config();
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
+// 로그를 나오게 하는 미들웨어
 app.use(morgan("dev"));
+// 정적인 파일들을 제공하는 라우터 역할을 하는 static
 app.use("/", express.static(path.join(__dirname, "public")));
+
+// 아래의 두 개는 body-parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// 쿠키 해석기
 app.use(cookieParser(process.env.COOKIE_SECRET));
+// 세션 관리용 미들웨어. 로그인 등의 이유로 세션을 구현하거나 특정 사용자를 위한 데이터를 임시적으로 저장해둘 때 유용
 app.use(
   session({
     resave: false,
@@ -26,6 +32,36 @@ app.use(
     name: "session-cookie",
   })
 );
+
+const multer = require("multer");
+const fs = require("fs");
+
+try {
+  fs.readdirSync("uploads");
+} catch (error) {
+  console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
+}
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+app.get("/upload", (req, res) => {
+  res.sendFile(path.join(__dirname, "multipart.html"));
+});
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log(req.file);
+  res.send("ok");
+});
+
 app.use((req, res, next) => {
   console.log("모든 요청에 전부 실행");
   next();

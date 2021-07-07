b61aad1d03fd7c1b099a5a44fd528844c3c44c0f
김지웅 <godjw96@gmail.com>
2021-07-07 14:25:46 +0900

    route

---
+++ nodeWorkbook/learn-express/routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+// GET/ 라우터
+
+router.get("/", (req, res) => {
+  res.send("Hello, Express");
+});
+
+module.exports = router;
